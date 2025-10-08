fe93253a5a856f9cd6eb988cb1cc1450520128b6
suparna chaudhari <154594764+Suparna62@users.noreply.github.com>
2025-10-08 16:13:42 +0530

    Update run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -9,12 +9,13 @@ def main():
     parser.add_argument("--question", type=str, help="Ask a custom question (overrides dataset mode)")
     args = parser.parse_args()
 
-    df = pd.read_csv(args.dataset, sep='\t')
+    df = pd.read_csv(args.dataset, sep='\t', header=None, names=["Question", "Q1", "Q2", "A1"])
 
     qa_model = load_qa_model()
-    
 
-    for i, row in df.head(args.n).iterrows():
+    if args.question:
+ 
+        row = df[df['Question'] == args.question].iloc[0]
         q = row['Question']
         q1 = row['Q1']
         q2 = row['Q2']
@@ -27,5 +28,20 @@ def main():
         a1 = answer_subquestions(q1, q2, qa_model)
         print(f"Answer: {actual}")
 
+    else:
+        for i, row in df.head(args.n).iterrows():
+            q = row['Question']
+            q1 = row['Q1']
+            q2 = row['Q2']
+            actual = row['A1']
+
+            print(f"\nQUESTION: {q}")
+            print(f"Q1: {q1}")
+            print(f"Q2: {q2}")
+
+            a1 = answer_subquestions(q1, q2, qa_model)
+            print(f"Answer: {actual}")
+
 if __name__ == "__main__":
     main()
+
